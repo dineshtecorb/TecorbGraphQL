ca2c940c923b9ba95f35bc908e66aed34aeeadf5
Dinesh <dinesh@tecorb.co>
2023-02-28 16:09:10 +0530

    Initial Commit

---
+++ TecorbGraphQL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TecorbGraphQL
+//
+//  Created by Dinesh Saini on 28/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
